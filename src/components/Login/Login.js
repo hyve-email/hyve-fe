10a5461b497233533e1f25b785f46577543f6a24
david-castaneda <dcast188@fiu.edu>
2019-07-26 14:35:20 -0500

    added localstorage

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,20 +5,42 @@ import PropTypes from 'prop-types';
 import Icon from '../Icon';
 import HyveLogo from '../../assets/hyve-logo.png';
 
-const responseGoogle = response => {
-  console.log(response);
+// API
+import UserService from '../../services/user';
+
+const responseGoogle = async response => {
+  const { Zi, googleId, profileObj } = response;
+  const { access_token } = Zi;
+  const { email, name, imageUrl } = profileObj;
+
+  const data = { email, name, imageUrl, googleId };
+
+  try {
+    await localStorage.setItem('googleToken', access_token);
+    await localStorage.setItem('googleId', googleId);
+    await localStorage.setItem('name', name);
+    await localStorage.setItem('email', email);
+    await localStorage.setItem('profile_image', imageUrl);
+
+    const response = await UserService.create(data);
+    console.log({ response });
+  } catch (err) {
+    console.log({ err });
+  }
 };
 
 const Login = props => {
   return (
     <LoginContainer>
-      <Icon size={100} icon={HyveLogo} />
-      <h3>Welcome to Hyve</h3>
-      <span>Liberate your email in minutes</span>
+      <Icon size={180} icon={HyveLogo} />
+      <h3>Welcome to your Hyve</h3>
+      <span style={{ fontSize: 25, fontWeight: '400', textAlign: 'center' }}>
+        Security, Safety, Organization <br /> all in one hyve.
+      </span>
       <GoogleWrapper>
         <GoogleLogin
-          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-          buttonText="Login"
+          clientId="497597479194-ahqalu2dcn6ggq2lrr5k0hov52dtr2uq.apps.googleusercontent.com"
+          buttonText="Continue with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
@@ -53,7 +75,7 @@ const LoginContainer = styled.div`
 
 const GoogleWrapper = styled.div`
   align-self: center;
-  width: 200px;
+
   margin-top: 25px;
 `;
 
